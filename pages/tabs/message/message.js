8ec5ccdb8422f4d792209d141813d424883c3605
徐泽朋 <zepeng1.xu@tcl.com>
2021-03-22 21:25:08 +0800

    前端消息页面渲染完成

--- pages/tabs/message/message.js
+++ pages/tabs/message/message.js
@@ -5,83 +5,89 @@ Page({
    * 页面的初始数据
    */
   data: {
-      active : 1,
-      isMsg : false,
-      woActive: 0,
-      msgActive: 1,
-      headActive: 0,
-      work_orders_all:[
-        {
-          aid : 123,
-          headLine : "健康码突然变红问题解决",
-          content : "您好，我想问一下我的健康码为什么突然变色了。我最近都在外地没有走动，是因为我们小区出事了吗？"
-        },
-        {
-          aid : 345,
-          headLine : "健康码突然变红问题解决",
-          content : "您好，我想问一下我的健康码为什么突然变色了。我最近都在外地没有走动，是因为我们小区出事了吗？"
-        },
-        {
-          aid : 346,
-          headLine : "健康码突然变红问题解决",
-          content : "您好，我想问一下我的健康码为什么突然变色了。我最近都在外地没有走动，是因为我们小区出事了吗？"
-        }
-      ],
-      unhandle:[
-        {
-          aid : 123,
-          headLine : "健康码突然变红问题解决",
-          content : "您好，我想问一下我的健康码为什么突然变色了。我最近都在外地没有走动，是因为我们小区出事了吗？"
-        },
-        {
-          aid : 345,
-          headLine : "健康码突然变红问题解决",
-          content : "您好，我想问一下我的健康码为什么突然变色了。我最近都在外地没有走动，是因为我们小区出事了吗？"
-        },
-        {
-          aid : 346,
-          headLine : "健康码突然变红问题解决",
-          content : "您好，我想问一下我的健康码为什么突然变色了。我最近都在外地没有走动，是因为我们小区出事了吗？"
-        }
-      ]
-  },
-  woChange:function(event) {
-      this.setData({
-        woActive : event.detail.index
-      })
-  },
-  headChange: function(event) {
-      this.setData({
-        headActive : event.detail.index
-      })
-      if(event.detail == 0) {
-        this.setData({
-          woActive : 1
-        })
-      }else{
-        this.setData({
-          msgActive : 0
-        })
+    active: 1,
+    isMsg: false,
+    woActive: 0,
+    msgActive: 1,
+    headActive: 0,
+    articles: [],
+    desc: '',
+    work_orders_all: [{
+        aid: 123,
+        headLine: "健康码突然变红问题解决",
+        content: "您好，我想问一下我的健康码为什么突然变色了。我最近都在外地没有走动，是因为我们小区出事了吗？"
+      },
+      {
+        aid: 345,
+        headLine: "健康码突然变红问题解决",
+        content: "您好，我想问一下我的健康码为什么突然变色了。我最近都在外地没有走动，是因为我们小区出事了吗？"
+      },
+      {
+        aid: 346,
+        headLine: "健康码突然变红问题解决",
+        content: "您好，我想问一下我的健康码为什么突然变色了。我最近都在外地没有走动，是因为我们小区出事了吗？"
       }
+    ],
+
   },
-
-
-
-
-  tabChange : function(event) {
+  woChange: function (event) {
+    let _this = this;
     this.setData({
-      active : event.detail
+      woActive: event.detail.index,
+      desc : _this.setDesc(0,event.detail.index)
+    })
+  },
+  headChange: function (event) {
+    this.setData({
+      headActive: event.detail.index
+    })
+    if (event.detail == 0) {
+      this.setData({
+        woActive: 1
+      })
+    } else {
+      this.setData({
+        msgActive: 0
+      })
+    }
+  },
+  tabChange: function (event) {
+    this.setData({
+      active: event.detail
     });
-    switch(event.detail) {
-      case 0 :
+    switch (event.detail) {
+      case 0:
         wx.redirectTo({
           url: '../../index/index',
         })
         break;
-        case 2:
+      case 2:
         wx.redirectTo({
           url: '../mine/mine',
         })
+    }
+  },
+  setDesc: function (upIdx, downIdx) {
+    if (upIdx == 0) {
+      switch (downIdx) {
+        case 0:
+          return '删除';
+        case 1:
+          return '退单';
+        case 2:
+          return '结单';
+        default:
+          return '删除'
+      }
+    } else {
+      switch (downIdx) {
+        case 0:
+          return '删除'
+        case 1:
+          return '已读'
+        default:
+          return '删除'
+      }
     }
   },
 
@@ -103,7 +109,12 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    // 发起请求
+    let _this = this;
+    this.setData({
+      articles: _this.data.work_orders_all,
+      desc: '删除'
+    })
   },
 
   /**
