ac99464e37cee39c10483b472368588da0a87c3a
poemyoung <2427389569@qq.com>
2021-04-13 15:05:32 +0800

    冷链追踪页面完成

--- pages/cold/chainfind/chainfind.js
+++ pages/cold/chainfind/chainfind.js
@@ -1,13 +1,41 @@
 // pages/cold/chainfind/chainfind.js
+import Toast from '../../../miniprogram_npm/@vant/weapp/toast/toast'
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    code :'',
+    cargo:{
+      source:'四川省达州市渠县',
+      id:'123',
+      class:'医药',
+      remark:'无',
+      places:[
+        '四川省达州市渠县',
+        '四川省达州市宣汉县',
+        '四川省成都市双流区'
+      ]
+    }
   },
-
+  scanQr:function() {
+      wx.scanCode({
+        onlyFromCamera: true,
+        scanType : ['qrCode'],
+        success:function(res) {
+          console.log(res);
+          let qr_res = res.result;
+         // 未完成 
+        },
+        fail:function(res) {
+            Toast.fail("扫码失败")
+        }
+      })
+  },
+  codeChange:function(event) {
+    console.log(this.data.code);
+  },
   /**
    * 生命周期函数--监听页面加载
    */
